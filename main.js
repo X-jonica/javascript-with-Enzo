f5c6b096054dd3cc486b12ae56a643e61df08379
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-04-05 10:02:31 +0300

    visualiser les données recu via API et pouvoir les utiliser

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-// comment faire un appel basique d'API avec XHR
+// Comment visualiser les données via notre API et pouvoir les utilisés apres ?
 
 const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://localhost:3000/api/pokemons");
+xhr.open("GET", "http://localhost:3000/api/language");
+
+xhr.onload = function () {
+  console.log(JSON.parse(xhr.response));
+};
 
 xhr.send();
